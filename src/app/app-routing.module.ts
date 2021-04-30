38c707e2f4b61d92224cef521d64242b5814bb1b
YutaMizusawa <yuta.mizusawa@dive.design>
2021-04-30 19:37:12 +0900

    ルーティングを使ってナビゲーションのリンクを追加

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+// アプリにルーティング機能を持たせることができる RouterModule と Routes をインポート
+import { RouterModule, Routes } from "@angular/router";
 
-const routes: Routes = [];
+// ルートを設定することでルーターに向かう場所を教えます
+import { HeroesComponent } from "./heroes/heroes.component";
+import { DashboardComponent } from "./dashboard/dashboard.component";
+
+const routes: Routes = [
+  { path: 'heroes', component: HeroesComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+];
 
 @NgModule({
+  // @NgModuleメタデータはルーターを初期化し
+  // ブラウザのロケーションの変更を待機します。
+  // forRootメソッドはルーティングに必要なサービス・プロバイダーとディレクティブを提供し、
+  // ブラウザの現在のURLを元に最初の遷移を行います。
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
